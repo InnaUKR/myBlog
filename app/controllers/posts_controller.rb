6815a4f0d1fba119781478f71614c3f7ccd72501
innaUKR <innascherbak11@gmail.com>
2017-03-28 17:37:40 +0300

    Create Post and Categories models

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+	def index
+		@content="From controller"
+	end
+
+	def edit
+
+	end
+
+	def delete 
+
+	end
+
+	def new
+
+	end
+
+	def update
+
+	end
+
+	def show
+
+	end
+end

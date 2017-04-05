ace988d260e0f42ae909275d98aee1db953073aa
innaUKR <innascherbak11@gmail.com>
2017-04-05 13:53:43 +0300

    Add Category create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,10 +10,10 @@ class CategoriesController < ApplicationController
   	@category =Category.new
   end
 
-  def update
-  	@category =Category.find(params[:id])
-  	if(@category.update_attributes(category_params))
-  		redirect_to categories_path, :notice=>"New category has added"
+  def create
+  	@category =Category.new(category_params)
+  	if(@category.save)
+  		redirect_to categories_path, :notice=>"New category has been added"
   	else
   		render "new"
   	end  			
